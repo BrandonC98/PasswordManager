801ee357a350efd260bf548a88225ade30c8fdec
Brandon Campbell <BrandonCampbell98@hotmail.co.uk>
2021-03-27 21:02:11 +0000

    Adds Context

--- PasswordManager/PasswordManager/Program.cs
+++ PasswordManager/PasswordManager/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
-
+using PasswordManagerData;
 using System.Net;
 using System.IO;
 
@@ -13,19 +13,37 @@ namespace PasswordManager
 
         static void Main(string[] args)
         {
-            var salt = Hash.GenerateSalt(20);
+            //var salt = Hash.GenerateSalt(20);
 
-            var hashPassword = Hash.GenerateHash(Encoding.ASCII.GetBytes("Password1"), salt, 1000, 16);
-            Console.WriteLine(Convert.ToBase64String(hashPassword));
+            //var hashPassword = Hash.GenerateHash(Encoding.ASCII.GetBytes("Password1"), salt, 1000, 16);
+            //Console.WriteLine(Convert.ToBase64String(hashPassword));
 
-            var hashPassword2 = Hash.GenerateHash(Encoding.ASCII.GetBytes("Password1"), salt, 1000, 16);
+            //var hashPassword2 = Hash.GenerateHash(Encoding.ASCII.GetBytes("Password1"), salt, 1000, 16);
 
-            Console.WriteLine("Password 1: " + Convert.ToBase64String(hashPassword));
-            Console.WriteLine("Password 2: " + Convert.ToBase64String(hashPassword2));
+            //Console.WriteLine("Password 1: " + Convert.ToBase64String(hashPassword));
+            //Console.WriteLine("Password 2: " + Convert.ToBase64String(hashPassword2));
 
 
-            if (Hash.CompareHash(hashPassword, hashPassword2)) Console.WriteLine("Login Successful");
-            else Console.WriteLine("Failed login");
+            //if (Hash.CompareHash(hashPassword, hashPassword2)) Console.WriteLine("Login Successful");
+            //else Console.WriteLine("Failed login");
+
+            using (var db = new PasswordmanagerContext())
+            {
+
+                db.websites.Add(new Website() { Name = "Google", Username = "me123" });
+                db.websites.Add(new Website() { Name = "YouTube", Username = "you123" });
+                db.websites.Add(new Website() { Name = "Facebook", Username = "Booo123" });
+
+                db.SaveChanges();
+
+                foreach (var i in db.websites)
+                {
+
+                    Console.WriteLine($"Id: {i.Id}, Name: {i.Name}, Username: {i.Username}");
+
+                }
+            }
+
 
         }
 
