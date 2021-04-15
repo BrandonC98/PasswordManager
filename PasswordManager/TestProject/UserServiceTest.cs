6149f9b8b34282a907a9a4b0ffaad3fb532d3fb9
Brandon Campbell <BrandonCampbell98@hotmail.co.uk>
2021-04-15 19:53:10 +0100

    Adds dependencies for mocks and in memory tests

---
+++ PasswordManager/TestProject/UserServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PasswordManagerData;
+using PasswordManagerData.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TestProject
+{
+    public class UserServiceTest
+    {
+
+        private UserService _sut;
+        private PasswordManagerContext _context;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+            //var option = new DbContextOptionsBuilder<PasswordManagerContext>().UseInMemoryDatabase(databaseName: "Example_DB")
+            //    .Options;
+
+        }
+
+    }
+}
